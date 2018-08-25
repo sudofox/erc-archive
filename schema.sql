835d7d86640ea9660ea4d19f2196e409c877deea
Austin Burk <apburk@sudomemo.net>
2018-08-25 15:58:10 -0400

    Tentative db schema and config

---
+++ schema.sql
@@ -0,0 +1,28 @@
+-- Schema for ERC Archive
+
+-- Speakers (Preachers, etc)
+
+CREATE TABLE `speakers` (
+ `speakerID` int(11) NOT NULL AUTO_INCREMENT,
+ `speakerName` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+ `speakerBio` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`speakerID`),
+  UNIQUE KEY `speakerName` (`speakerName`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Audio recordings
+-- File path: /campmeetingYear/filename
+
+CREATE TABLE `audio` (
+ `audioID` int(11) NOT NULL AUTO_INCREMENT,
+ `length` int(11) NOT NULL COMMENT 'Length of the recording in seconds',
+ `filename` varchar(128) NOT NULL,
+ `speakerID` int(11) DEFAULT NULL COMMENT 'If associated with a speaker, this is their primary key',
+ `campmeetingYear` int(4) NOT NULL COMMENT '4-digit year',
+ `title` varchar(128) DEFAULT NULL COMMENT 'Title of recording, if null then show filename',
+ `description` text DEFAULT NULL,
+ PRIMARY KEY (`audioID`),
+ UNIQUE KEY `filename` (`filename`),
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
